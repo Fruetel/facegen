29c640a6da284ae72efcc6cbe5997318aeed80fe
Thomas Fruetel <github@fruetel.de>
2019-03-26 17:09:57 +0100

    Add image generation code

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import time
 import os
 import sys
 import pickle
@@ -8,3 +9,20 @@ import dnnlib.tflib as tflib
 import config
 
 tflib.init_tf()
+
+# Load pre-trained network.
+url = 'https://drive.google.com/uc?id=1MEGjdvVpUsu1jB4zrXZN7Y4kBBOzizDQ'
+with dnnlib.util.open_url(url, cache_dir="/tmp/cache") as f:
+    _G, _D, Gs = pickle.load(f)
+
+# Pick latent vector.
+seed = round(time.time())
+
+rnd = np.random.RandomState(seed)
+latents = rnd.randn(1, Gs.input_shape[1])
+
+fmt = dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=True)
+
+images = Gs.run(latents, None, truncation_psi=0.7, randomize_noise=True, output_transform=fmt)
+
+print("Done.")
